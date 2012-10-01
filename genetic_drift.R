abae64342685453f60fffa94f729f0d4001b48f8
Mike Covington <mfcovington@gmail.com>
2012-10-01 14:36:58 -0700

    cleanup

--- genetic_drift.R
+++ genetic_drift.R
@@ -17,14 +17,16 @@ DoSim <- function(pops, size, generations, freq, out, returnTable = FALSE, write
   #
   # Returns:
   #
+
+  # run simulation
   size <- size * 2  # assume individuals are diploid but there are 2n chromosomes
   results <- matrix(nrow = generations + 1, ncol = pops)  # create matrix to hold results
   results[1, ] <- freq  # populate first row with initial frequencies
   for (g in 2:(generations + 1)) {  # loop through the generations
     results[g, ] <- rbinom(pops, size, prob = results[g - 1, ]) / size  # calculate the next generations frequencies
   }
-  colnames(results) <- paste("Pop_", 1:ncol(results), sep = "")
-  rownames(results) <- paste("Gen_", 0:(nrow(results) - 1), sep = "")
+
+  # output plot
   setwd("public")  # hard-coded for now, because not working any other way
   png_out <- paste( out, "gen_drift.png", sep = "/")
   png(png_out)
@@ -35,6 +37,10 @@ DoSim <- function(pops, size, generations, freq, out, returnTable = FALSE, write
                 ylab = " frequency",
                 ylim = c(0, 1)))
   dev.off()
+
+  # output table
+  colnames(results) <- paste("Pop_", 1:ncol(results), sep = "")
+  rownames(results) <- paste("Gen_", 0:(nrow(results) - 1), sep = "")
   if (returnTable) results  # return results if requested
   table_out <- paste( out, "gen_drift.tsv", sep = "/")
   if (writeTable) write.table(results, file = table_out, quote = F, sep='\t')
